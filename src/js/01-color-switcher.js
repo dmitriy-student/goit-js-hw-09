799c1b30fe21d6684995ea3b6904d21ed3460ad6
dmitriy-student <dima.ermol22@gmail.com>
2023-04-30 17:49:00 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+const delay = 1000;
+let timerId = null;
 
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, delay);
+}
+
+function onStopBtn() {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
